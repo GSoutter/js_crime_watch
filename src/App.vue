c017197d3d42c04926a72641910f8b83ed7a2fc3
Gordon Soutter <gordon.soutter@outlook.com>
2020-04-25 12:56:52 +0100

    Initial commit. adds file and developer quote

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,33 @@
-<template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+<template lang="html">
+  <div>
+    <h1>Crime Watch</h1>
+    <developer-quote></developer-quote>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import DeveloperQuote from './components/DeveloperQuote.vue'
+
+
 
 export default {
-  name: 'App',
+  name: 'app',
+  date() {
+    return {
+    crimeData: [],
+    developerQuote: ""
+    }
+  },
+  mounted (){
+
+  },
   components: {
-    HelloWorld
+    'developer-quote': DeveloperQuote
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="css" scoped>
 </style>
---
+++ src/components/DeveloperQuote.vue
@@ -0,0 +1,43 @@
+<template lang="html">
+  <div>
+
+    <p>Developer Quote:</p>
+    <p>"{{developerQuote}}"</p>
+    <p>{{author}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "developer-quote",
+  data() {
+    return {
+      developerQuote: '',
+      author: ''
+    }
+  },
+  mounted() {
+    fetch('https://programming-quotes-api.herokuapp.com/quotes/random')
+    .then(res => res.json())
+    .then(data => {
+      this.developerQuote = data.en
+      this.author = data.author
+    })
+
+  }
+
+
+
+}
+
+</script>
+
+<style lang="css" scoped>
+div {
+  float: right;
+  width: 200px;
+  border: solid;
+  padding: 4px;
+}
+
+</style>
